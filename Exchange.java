33b191723ddaa07f381557fe9b4bd1604b3c907a
ji <37184303+jikwjjw@users.noreply.github.com>
2020-06-17 11:18:14 +0800

    Update Exchange.java

--- Exchange.java
+++ Exchange.java
@@ -1,8 +1,8 @@
 package mask;
 
 /**
- * ���дһ�������������ڲ�ʹ���κ���ʱ������ֱ�ӽ�����������ֵ�� 
- * ����һ��int����AB��������Ԫ�غ͵�һ��Ԫ��Ϊ��������ֵ���뷵�ؽ���������顣
+ * 请编写一个函数，函数内不使用任何临时变量，直接交换两个数的值。 
+ * 给定一个int数组AB，其第零个元素和第一个元素为待交换的值，请返回交换后的数组。
  * 
  * @author ji
  *
@@ -14,5 +14,14 @@ public class Exchange {
 		AB[1] = AB[0] ^ AB[1];
 		AB[0] = AB[0] ^ AB[1];
 		return AB;
+		
+		/** 
+		//第二种方法
+		AB[1] = AB[0] - AB[1];
+		AB[0] = AB[0] - AB[1];
+		AB[1] = AB[0] + AB[1];
+		return AB;
+		* 
+		*/
 	}
 }
