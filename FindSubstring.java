cdc4b4caf878dc708059722d6d558ed84f90d46e
ji <37184303+jikwjjw@users.noreply.github.com>
2020-07-04 22:02:14 +0800

    字符串匹配
    给定一个字符串 s 和一些长度相同的单词 words。找出 s 中恰好可以由 words 中所有单词串联形成的子串的起始位置。
    
    注意子串要与 words 中的单词完全匹配，中间不能有其他字符，但不需要考虑 words 中单词串联的顺序。

---
+++ FindSubstring.java
@@ -0,0 +1,75 @@
+package string;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * 给定一个字符串 s 和一些长度相同的单词 words。找出 s 中恰好可以由 words 中所有单词串联形成的子串的起始位置。
+ * 注意子串要与 words 中的单词完全匹配，中间不能有其他字符，但不需要考虑 words 中单词串联的顺序。
+ *
+ */
+public class FindSubstring {
+    /**
+     *
+     * @param s s = "barfoothefoobarman"
+     * @param words words = ["foo","bar"]
+     * @return [0,9] 从索引 0 和 9 开始的子串分别是 "barfoo" 和 "foobar" 。输出的顺序不重要, [9,0] 也是有效答案。
+     *
+     */
+
+    /**
+     *
+     * @param s s = "wordgoodgoodgoodbestword",
+     * @param words  words = ["word","good","best","word"]
+     * @return []
+     */
+    public static List<Integer> findSubstring(String s, String[] words) {
+        List<Integer> res = new ArrayList<Integer>();
+        int wordNum = words.length;
+        if (wordNum == 0) {
+            return res;
+        }
+        int wordLen = words[0].length();
+
+        HashMap<String, Integer> allWords = new HashMap<String, Integer>();
+        for (String w : words) {
+            int value = allWords.getOrDefault(w, 0);
+            allWords.put(w, value + 1);
+        }
+
+        for (int i = 0; i < s.length() - wordNum * wordLen + 1; i++) {
+            //HashMap2 存当前扫描的字符串含有的单词
+            HashMap<String, Integer> hasWords = new HashMap<String, Integer>();
+            int num = 0;
+            //判断该子串是否符合
+            while (num < wordNum) {
+                String word = s.substring(i + num * wordLen, i + (num + 1) * wordLen);
+                //判断该单词在 HashMap1 中
+                if (allWords.containsKey(word)) {
+                    int value = hasWords.getOrDefault(word, 0);
+                    hasWords.put(word, value + 1);
+                    //判断当前单词的 value 和 HashMap1 中该单词的 value
+                    if (hasWords.get(word) > allWords.get(word)) {
+                        break;
+                    }
+                } else {
+                    break;
+                }
+                num++;
+            }
+            //判断是不是所有的单词都符合条件
+            if (num == wordNum) {
+                res.add(i);
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        String s = "barfoothefoobarman";
+        String[] words = {"foo", "bar"};
+        System.out.println(FindSubstring.findSubstring(s, words));
+
+    }
+}
