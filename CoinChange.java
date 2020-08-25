453bdafdb296e17745ef789a9e2d6010349fc23e
ji <37184303+jikwjjw@users.noreply.github.com>
2020-08-25 21:35:27 +0800

    Create CoinChange.java

---
+++ CoinChange.java
@@ -0,0 +1,28 @@
+package letcode;
+
+import java.util.Arrays;
+
+/**
+ * 给定不同面额的硬币 coins 和一个总金额 amount。编写一个函数来计算可以凑成总金额所需的最少的硬币个数。如果没有任何一种硬币组合能组成总金额，返回 -1。
+ * <p>
+ * 输入: coins = [1, 2, 5], amount = 11
+ * 输出: 3
+ * 解释: 11 = 5 + 5 + 1
+ */
+public class CoinChange {
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        Arrays.sort(coins);
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < coins.length; j++) {
+                if (coins[j] <= i) {
+                    dp[i] = Math.min(dp[i - coins[j]] + 1, dp[i]);
+                }
+
+            }
+        }
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+}
