5cc2c358fece01f47ab40383eeb06ac0c87507ac
ji <37184303+jikwjjw@users.noreply.github.com>
2020-08-26 20:34:41 +0800

    Create SubarraySum.java

---
+++ SubarraySum.java
@@ -0,0 +1,34 @@
+package letcode.String;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 前缀和 + 哈希表优化
+ * <p>
+ * 给定一个整数数组和一个整数 k，你需要找到该数组中和为 k 的连续的子数组的个数。
+ * <p>
+ * 示例 1 :
+ * <p>
+ * 输入:nums = [1,1,1], k = 2
+ * 输出: 2 , [1,1] 与 [1,1] 为两种不同的情况。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/subarray-sum-equals-k
+ */
+public class SubarraySum {
+    public static int subarraySum(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        int sum = 0;
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (map.containsKey(sum - k)) {
+                count += map.get(sum - k);
+            }
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        return count;
+    }
+}
