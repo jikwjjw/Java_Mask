bfaceaab344a44fa431ba9d68e093e5288466cf6
ji <37184303+jikwjjw@users.noreply.github.com>
2020-08-26 21:13:45 +0800

    Create FindUnsortedSubarray.java

---
+++ FindUnsortedSubarray.java
@@ -0,0 +1,30 @@
+package letcode.String;
+
+import java.util.Arrays;
+
+/**
+ * 最短无序连续子数组
+ * 给定一个整数数组，你需要寻找一个连续的子数组，如果对这个子数组进行升序排序，那么整个数组都会变为升序排序。
+ * <p>
+ * 你找到的子数组应是最短的，请输出它的长度。
+ * <p>
+ * 输入: [2, 6, 4, 8, 10, 9, 15]
+ * 输出: 5
+ * 解释: 你只需要对 [6, 4, 8, 10, 9] 进行升序排序，那么整个表都会变为升序排序。
+ * <p>
+ */
+public class FindUnsortedSubarray {
+    public int findUnsortedSubarray(int[] nums) {
+        int[] snums = nums.clone();
+        Arrays.sort(snums);
+        int start = snums.length;
+        int end = 0;
+        for (int i = 0; i < snums.length; i++) {
+            if (snums[i] != nums[i]) {
+                start = Math.min(start, i);
+                end = Math.max(end, i);
+            }
+        }
+        return (end - start >= 0 ? end - start + 1 : 0);
+    }
+}
